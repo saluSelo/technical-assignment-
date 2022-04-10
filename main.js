008d013617daa9df4a345137333ba86264be5222
saluSelo <76534981+saluSelo@users.noreply.github.com>
2022-04-10 16:25:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+/*const keyBoard = document.querySelector('.form-control') 
+
+keyBoard.addEventListener('keyup', e => {
+
+console.log(e)
+}) 
+
+const keyBoard2 = document.querySelector('.form-control2') 
+
+keyBoard2.addEventListener('keyup', e => {
+
+console.log(e)
+}) 
+*/
+document.getElementById('Buttonsubmit').onclick = function() {
+
+    var myname =document.getElementById('text1btn1').value;
+    console.log(myname)
+}
